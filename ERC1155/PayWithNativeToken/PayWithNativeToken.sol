b933e7dd700a18e277116770a5a219a900ec9469
ShahidBMD <shahidbuildmydapp665@gmail.com>
2023-03-30 09:26:04 +0500

    minor changes

--- ERC1155/PayWithNativeToken/PayWithNativeToken.sol
+++ ERC1155/PayWithNativeToken/PayWithNativeToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract PayWithNativeToken is ERC1155,Ownable {
     constructor() ERC1155("") {}
